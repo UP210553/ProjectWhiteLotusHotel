9986f81ec5531a4315c49ae838b00efc4faf828d
root <root@localhost.localdomain>
2023-07-02 01:42:56 -0600

    Adición de elementos CSS, archivos PHP e imagen

---
+++ Datos/conexion.php
@@ -0,0 +1,7 @@
+<?php 
+    $host = "localhost";
+    $database = "hotel";
+    $user = "d2023p";
+    $password = "RG-1905032121118826";
+    $conection = mysqli_connect($host, $database, $user, $password) or die("Couldn't connect to" . $host . "/" . $database . "/");
+?>

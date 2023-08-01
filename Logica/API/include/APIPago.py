206d438675adae44b30fba0120987104d1b4cbbe
root <root@localhost.localdomain>
2023-08-01 16:09:06 -0600

    API pagos

---
+++ Logica/API/include/APIPago.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import mysql.connector, os
+from dotenv import load_dotenv
+
+load_dotenv()
+host = os.getenv('host')
+user = os.getenv('user')
+password = os.getenv('password')
+database = os.getenv('database')
+
+config = {
+    'user': user,
+    'password': password,
+    'host': host,
+    'database': database,
+    'raise_on_warnings': True,
+}
+
+mydb = mysql.connector.connect(**config)
+
+mycursor=mydb.cursor()
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route('/pago',methods=['GET', 'POST'])
+def hacerPago():
+    try:
+        cuenta = request.json['cuenta']
+        clave = request.json['clave']
+        mycursor.execute("SELECT Id FROM Clientes WHERE cuenta \
+                          = " + cuenta + " AND clave = " + clave)
+        myresult = mycursor.fetchone()
+        if myresult == None or myresult == "":
+            return str("Cuenta no registrada")
+        else:
+            return str(myresult)
+    except NameError as msg:
+        print(msg)
+    else:
+        print("Sin conexión")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Logica/JS/conection.js
@@ -0,0 +1,16 @@
+const submit = async (e) => {
+    function Hola() {
+        alert('Hola mundo');
+    }
+    // e.preventDefault(
+    // const res = fetch(`${API}/pago`,{
+    //     method:'POST',
+    //     headers:{
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //         cuenta,
+    //         clave
+    //     })
+    // })
+}

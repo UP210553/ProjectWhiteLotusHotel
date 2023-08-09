65bee41d7fce009cfd0de38c82520694633bc690
UP210553 <up210553@alumnos.upa.edu.mx>
2023-08-08 21:49:13 -0600

    Clase

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
 </head>
 <body>
 <div>
@@ -19,7 +20,7 @@
 </div>
 
 <?php 
-include("Logico/VerificarCredenciales.php" );
+require_once __DIR__."/Logico/VerificarCredenciales.php";
 $inicioSecion = VerificarCredenciales();
 if($inicioSecion==true){
 ?>
